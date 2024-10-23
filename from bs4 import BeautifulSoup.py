31e5737c5380d05720c6b91649addd3eb0fe573d
Rebecca Fiestas <rebecca.fiestas@gmail.com>
2024-10-23 12:05:28 +0200

    feature to extract dates and journals added

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -19,3 +19,11 @@ def extract_autor():
         autor_text = autor.get_text(strip= True)
         print(f"{index2}. {autor_text}")
 
+meta_dates = soup.find_all('div', class_="meta__details")
+for i3, meta_date in enumerate(meta_dates, 1):
+    this_soup = BeautifulSoup(str(meta_date), 'xml')
+    journal = this_soup.find('a', class_="meta__serial")
+    journal_text = journal.get_text(strip=True)
+    date = this_soup.find('span', class_="meta__epubDate")
+    date_text = date.get_text(strip=True)if date else "Not found"
+    print(f"{i3}. journal: {journal_text}, date: {date_text}")
