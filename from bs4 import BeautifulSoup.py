8594c9550c64f402d98dc9c8d89aa578b99025cf
Rebecca Fiestas <rebecca.fiestas@gmail.com>
2024-10-23 10:50:27 +0200

    title extracted

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
-with open ("html_doc.html", encoding='utf-8')as file: 
+with open ("html_doc3.html", encoding='utf-8')as file: 
     soup = BeautifulSoup(file, 'xml')   
 
-print(soup.title)
+titles = soup.find_all('h4', class_="meta__title meta__title__margin")
+
+# Extract and print the titles
+for index, title in enumerate(titles, 1):
+    title_text = title.get_text(strip=True)
+    print(f"{index}. {title_text}")
+
+
