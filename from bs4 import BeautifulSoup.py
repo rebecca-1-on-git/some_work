643f00d87ff379d7b8df252a08c6325106245e87
Rebecca Fiestas <rebecca.fiestas@gmail.com>
2024-10-23 11:17:16 +0200

    function extract autors added

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -2,11 +2,20 @@ from bs4 import BeautifulSoup
 with open ("html_doc3.html", encoding='utf-8')as file: 
     soup = BeautifulSoup(file, 'xml')   
 
-titles = soup.find_all('h4', class_="meta__title meta__title__margin")
+def extract_title():
+    titles = soup.find_all('h4', class_="meta__title meta__title__margin")
 
-# Extract and print the titles
-for index, title in enumerate(titles, 1):
-    title_text = title.get_text(strip=True)
-    print(f"{index}. {title_text}")
+    # Extract and return the titles
+    for index, title in enumerate(titles, 1):
+        title_text = title.get_text(strip=True)
+        return f"{index}. {title_text}"
 
+
+def extract_autor():
+    autors = soup.find_all('ul', class_="meta__authors rlist--inline" )
+
+
+    for index2, autor in enumerate(autors, 1):
+        autor_text = autor.get_text(strip= True)
+        print(f"{index2}. {autor_text}")
 
